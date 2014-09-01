7daf0fc7e97c240b032e36d12fafc219482fdeb0
Mads Kristensen <post@madskristensen.net>
2014-09-01 13:54:53 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿module.exports = function (grunt) {
+
+    require('load-grunt-config')(grunt, {
+        jitGrunt: true
+    });
+
+    grunt.config.set("jshint.options.reporter", require('jshint-stylish'));
+
+}
